a5e235f5f5997910a21d3291fd9e0d0e778d3be9
bomberkill <ronaldkamgaing4@gmail.com>
2024-06-12 10:06:49 +0100

    Apply Jest test

---
+++ components/Welcome/Welcome.tsx
@@ -0,0 +1,28 @@
+import { Anchor, Text, Title } from '@mantine/core';
+
+import { useTranslation } from 'next-i18next';
+import React from 'react';
+import classes from './Welcome.module.css';
+
+export function Welcome() {
+  const { t } = useTranslation('common');
+
+  return (
+    <>
+      <Title className={classes.title} ta="center" mt={100}>
+        Welcome to{' '}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+          {t('acceuil')}
+        </Text>
+      </Title>
+      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+        This starter Next.js project includes a minimal setup for server side rendering, if you want
+        to learn more on Mantine + Next.js integration follow{' '}
+        <Anchor href="https://mantine.dev/guides/next/" size="lg">
+          this guide
+        </Anchor>
+        . To get started edit index.tsx file.
+      </Text>
+    </>
+  );
+}
